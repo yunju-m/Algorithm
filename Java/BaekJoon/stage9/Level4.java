f51d9e6b9538a827cd55daf30188b0e25d6393b2
yunju-m <74498379+yunju-m@users.noreply.github.com>
2022-03-07 17:42:34 +0900

    Add files via upload

---
+++ Java/BaekJoon/stage9/Level4.java
@@ -0,0 +1,29 @@
+package stage9;
+
+import java.util.Scanner;
+// 소수 구하기
+// 에스토스테네스체 사용
+public class Level4 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int M = scn.nextInt(), N = scn.nextInt();
+		getPrime(M, N);
+
+		scn.close();
+	}
+
+	public static void getPrime(int m, int n) {
+		int[] primeChk = new int[n+1];
+		primeChk[0] = primeChk[1] = 1;
+
+		for (int i = 2; i <=n; i++) {
+			for (int j = 2; i*j<=n; j++) {
+				primeChk[i*j] = 1;
+			}
+		}
+		for (int i = m; i <= n; i++) {
+			if (primeChk[i] != 1)
+				System.out.println(i);
+		}
+	}
+}
