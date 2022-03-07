5f6f87b0bbbf680ce8994a54b0989ae7ccd913d5
yunju-m <74498379+yunju-m@users.noreply.github.com>
2022-03-07 17:39:53 +0900

    Add files via upload

---
+++ Java/BaekJoon/stage1/Level10.java
@@ -0,0 +1,12 @@
+package stage1;
+
+import java.util.Scanner;
+public class Level10 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		System.out.println(str+"??!");
+		
+		scn.close();
+	}
+}
