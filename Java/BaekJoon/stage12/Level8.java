b7b86894c8571662f8a80269e71acef6bdd39f7d
yunju-m <74498379+yunju-m@users.noreply.github.com>
2022-03-28 16:13:22 +0900

    Add files via upload

---
+++ Java/BaekJoon/stage12/Level8.java
@@ -0,0 +1,19 @@
+package stage12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 단어 정렬
+public class Level8 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			
+		}
+		
+	}
+}
