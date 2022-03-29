3610217962fca1d36bba5cec8e143743c1db835c
yunju-m <74498379+yunju-m@users.noreply.github.com>
2022-03-29 17:36:40 +0900

    Add files via upload

--- BaekJoon/stage11/Level8.java
+++ BaekJoon/stage11/Level8.java
@@ -10,10 +10,39 @@ public class Level8 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
-		
+		boolean[] mul = new boolean[N];
+		String[] s = new String[N];
 		for (int i = 0; i < N; i++) {
-			
+			s[i] = br.readLine();
 		}
-		
+
+		for (int i = 0; i < N; i++) {
+			mul[i] = false;
+			for (int j = i+1; j < N; j++) {
+				if (s[i].length() > s[j].length()) {
+					String cnt = s[i];
+					s[i] = s[j];
+					s[j] = cnt;
+					mul[i] = false;
+				}
+				else if (s[i].length()==s[j].length()) {
+					if(s[i].compareTo(s[j]) > 0) {
+						String cnt = s[i];
+						s[i] = s[j];
+						s[j] = cnt;
+						mul[i] = false;
+					} else if (s[i].compareTo(s[j]) == 0) {
+						mul[i] = true;
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (mul[i] != true) {
+				sb.append(s[i]+"\n");
+			}
+		}
+		System.out.println(sb);
 	}
 }
