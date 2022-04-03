663d8bc2e779227669cbc85b94917fdd53f485de
yunju-m <74498379+yunju-m@users.noreply.github.com>
2022-04-03 23:26:29 +0900

    Add files via upload

---
+++ BaekJoon/stage11/Level10.java
@@ -0,0 +1,39 @@
+package stage11;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Scanner;
+
+// 좌표 압축
+// 입력받은 수에서 작은 수가 몇개인지 각각 출력하는 문제
+public class Level10 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int N = scn.nextInt();
+		ArrayList<Integer> a = new ArrayList<>();
+
+		for (int i = 0; i < N; i++) {
+			a.add(scn.nextInt());
+		}
+		// 중복값 제거
+		HashSet<Integer> arr2 = new HashSet<Integer>(a);
+		ArrayList<Integer> a2 = new ArrayList<>(arr2);
+		Collections.sort(a2);
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < a2.size(); j++) {
+				if (a.get(i).compareTo(a2.get(j))==0) {
+					a.add(i, j);
+					a.remove(i+1);
+				}
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			System.out.print(a.get(i) + " ");
+		}
+		scn.close();
+	}
+
+}
