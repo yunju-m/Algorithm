dff768582e9c7fbc5c6f556f829d309cee050f23
yunju-m <74498379+yunju-m@users.noreply.github.com>
2022-04-24 00:47:46 +0900

    Add files via upload

---
+++ BaekJoon/bronzeLevel/N11050.java
@@ -0,0 +1,24 @@
+package bronzeLevel;
+
+import java.util.Scanner;
+
+// 이항 계수 1
+public class N11050 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int N = scn.nextInt();
+		int K = scn.nextInt();
+		System.out.println(factorial(N)/(factorial(K)*factorial(N-K)));
+		scn.close();
+	}
+
+	public static int factorial(int n) {
+		int sum = 1;
+		int size = n;
+		for (int i = 0; i < size; i++) {
+			sum *= n;
+			n--;
+		}
+		return sum;
+	}
+}
