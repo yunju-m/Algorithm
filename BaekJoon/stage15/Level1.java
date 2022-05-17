6a5d7af4d2342daae39bd2da179908668e92ffcf
yunju-m <74498379+yunju-m@users.noreply.github.com>
2022-05-17 16:37:45 +0900

    Add files via upload

---
+++ BaekJoon/stage15/Level1.java
@@ -0,0 +1,40 @@
+package stage15;
+
+import java.util.Scanner;
+
+// 백트레킹
+// N과 M(1)
+public class Level1 {
+	public static int[] arr;
+	public static boolean[] visit;
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int N = scn.nextInt(), M = scn.nextInt();
+		arr = new int[M];
+		visit = new boolean[N];
+		dfs(N, M, 0);
+		scn.close();
+	}
+
+	public static void dfs(int N, int M, int depth) {
+		if (depth == M) {
+			for (int val : arr) {
+				System.out.print(val + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			if (visit[i] == false) {
+				visit[i] = true;
+				arr[depth] = i+1;
+				dfs(N, M, depth+1);
+				
+				visit[i] = false;
+			}
+		}
+		return;
+	}
+}
---
+++ BaekJoon/stage15/Level3.java
@@ -0,0 +1,35 @@
+package stage15;
+
+import java.util.Scanner;
+
+// 백트래킹
+// N과 M(3)
+public class Level3 {
+	public static int[] arr;
+	public static int N, M;
+	public static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		N = scn.nextInt();
+		M = scn.nextInt();
+		arr = new int[M];
+		dfs(0);
+		System.out.print(sb);
+		scn.close();
+	}
+
+	public static void dfs(int depth) {
+		if (depth == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(arr[i] +" ");
+			}
+			sb.append('\n');
+			return;
+		}
+		for (int i = 1; i <= N; i++) {
+			arr[depth] = i;
+			dfs(depth+1);
+		}
+	}
+
+}
