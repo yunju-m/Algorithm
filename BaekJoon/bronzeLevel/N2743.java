29358b5e65e3127b95d0d7d8eea76261caa17bcc
yunju-m <74498379+yunju-m@users.noreply.github.com>
2022-06-07 09:17:33 +0900

    Add files via upload

---
+++ BaekJoon/bronzeLevel/N2743.java
@@ -0,0 +1,14 @@
+package bronzeLevel;
+
+import java.util.Scanner;
+
+// 단어 길이 재기
+public class N2743 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String s = scn.next();
+		System.out.println(s.length());
+		scn.close();
+	}
+
+}
---
+++ BaekJoon/bronzeLevel/N2754.java
@@ -0,0 +1,29 @@
+package bronzeLevel;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+// 학점계산
+public class N2754 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		HashMap<String, Double> map = new HashMap<>();
+		map.put("A+", 4.3);
+		map.put("A0", 4.0);
+		map.put("A-", 3.7);
+		map.put("B+", 3.3);
+		map.put("B0", 3.0);
+		map.put("B-", 2.7);
+		map.put("C+", 2.3);
+		map.put("C0", 2.0);
+		map.put("C-", 1.7);
+		map.put("D+", 1.3);
+		map.put("D0", 1.0);
+		map.put("D-", 0.7);
+		map.put("F", 0.0);
+		String s = scn.nextLine();
+		System.out.println(map.get(s));
+		scn.close();
+	}
+
+}
