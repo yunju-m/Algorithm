9af591fb678d47b393823c6a2d0bccc233fcde70
yunju-m <74498379+yunju-m@users.noreply.github.com>
2022-06-09 16:06:10 +0900

    Add files via upload

---
+++ BaekJoon/bronzeLevel/N9086.java
@@ -0,0 +1,21 @@
+package bronzeLevel;
+
+import java.util.Scanner;
+
+// 문자열
+public class N9086 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		int T = scn.nextInt();
+		String s;
+
+		for (int i = 0; i < T; i++) {
+			s = scn.next();
+			sb.append(s.charAt(0)+""+s.charAt(s.length()-1)).append('\n');
+		}
+		System.out.println(sb);
+		scn.close();
+	}
+
+}
