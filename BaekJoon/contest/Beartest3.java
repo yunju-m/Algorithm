5cd485ba18514a2d8e391b96cf5a10840037aa39
yunju-m <74498379+yunju-m@users.noreply.github.com>
2022-06-23 11:58:00 +0900

    Add files via upload

---
+++ BaekJoon/contest/Beartest3.java
@@ -0,0 +1,24 @@
+package contest;
+
+import java.util.Scanner;
+
+// 곰곰이의 식단 관리
+public class Beartest3 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int N = scn.nextInt();
+		int cnt = 0;
+		String s = scn.next();
+		char c;
+		for (int i = 0; i < N; i++) {
+			c = s.charAt(i);
+			if (c == 'C') {
+				cnt+=1;
+			}
+		} 
+		System.out.println(cnt/(N-cnt+1)+(cnt%(N-cnt+1)!=0?1:0));
+
+		scn.close();
+	}
+
+}
