b2084c8ca580b4dc389a447ab5db1fd55318cff2
yunju-m <74498379+yunju-m@users.noreply.github.com>
2022-07-01 11:11:17 +0900

    Add files via upload

---
+++ BaekJoon/bronzeLevel/N10039.java
@@ -0,0 +1,20 @@
+package bronzeLevel;
+
+import java.util.Scanner;
+
+public class N10039 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int sum = 0;
+		int n;
+		for (int i = 0; i < 5; i++) {
+			n = scn.nextInt();
+			if (n < 40) {
+				sum += 40;
+			} else sum += n;
+		}
+		System.out.println(sum/5);
+		scn.close();
+	}
+
+}
