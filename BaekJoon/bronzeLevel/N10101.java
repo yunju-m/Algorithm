2574902c7dc7d89741c03973932ed8a114000778
yunju-m <74498379+yunju-m@users.noreply.github.com>
2022-07-08 17:18:05 +0900

    Add files via upload

---
+++ BaekJoon/bronzeLevel/N10101.java
@@ -0,0 +1,23 @@
+package bronzeLevel;
+
+import java.util.Scanner;
+
+// 삼각형 외우기
+public class N10101 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int A = scn.nextInt();
+		int B = scn.nextInt();
+		int C = scn.nextInt();
+		if (A == 60 && B == 60 && C == 60)
+			System.out.println("Equilateral");
+		else if ((A+B+C)==180 &&(A==B || B==C || A==C))
+			System.out.println("Isosceles");
+		else if ((A+B+C)==180 &&((A!=B) || (A!=C) || (B!=C)))
+			System.out.println("Scalene");
+		else 
+			System.out.println("Error");
+		scn.close();
+	}
+
+}
