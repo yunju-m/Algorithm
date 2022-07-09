918fefa40a54cbe4b3a78da08505922d9757892d
yunju-m <74498379+yunju-m@users.noreply.github.com>
2022-07-09 16:00:13 +0900

    Add files via upload

---
+++ BaekJoon/N10156.java
@@ -0,0 +1,17 @@
+package bronzeLevel;
+
+import java.util.Scanner;
+
+// 과자
+public class N10156 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int K = scn.nextInt();
+		int N = scn.nextInt();
+		int M = scn.nextInt();
+		if (M-K*N < 0) System.out.println(K*N-M);
+		else System.out.println(0);
+		scn.close();
+	}
+
+}
