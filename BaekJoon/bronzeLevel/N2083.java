c1903f5e66e5ef60e2876f63792dc099d2370b28
yunju-m <74498379+yunju-m@users.noreply.github.com>
2022-07-21 15:38:50 +0900

    Add files via upload

---
+++ BaekJoon/bronzeLevel/N2083.java
@@ -0,0 +1,29 @@
+package bronzeLevel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 럭비 클럽
+public class N2083 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		String s; int age, weight;
+		while (true) {
+			st = new StringTokenizer(br.readLine(), " ");
+			s = st.nextToken();
+			if (s.equals("#")) break;
+			else {
+				age = Integer.parseInt(st.nextToken());
+				weight = Integer.parseInt(st.nextToken());
+				if (age > 17 || weight >= 80) 
+					sb.append(s +" " + "Senior").append('\n');
+				else sb.append(s + " " + "Junior").append('\n');
+			}
+		}
+		System.out.println(sb);
+	}
+}
