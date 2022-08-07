dc2fa4bae6830a6d0e7ae0af8d20dc3c4f7fc32e
yunju-m <74498379+yunju-m@users.noreply.github.com>
2022-08-07 19:12:13 +0900

    Add files via upload

---
+++ BaekJoon/bronzeLevel/N11948.java
@@ -0,0 +1,28 @@
+package bronzeLevel;
+
+import java.util.Scanner;
+
+// 과목선택
+public class N11948 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int[] arr = new int[6];
+		int min = 100;
+		int sum = 0;
+		for (int i = 0; i < 4; i++) {
+			arr[i] = scn.nextInt();
+			sum += arr[i];
+			min = Math.min(arr[i], min);
+		}
+		sum -= min;
+		min = 100;
+		for (int i = 0; i < 2; i++) {
+			arr[i] = scn.nextInt();
+			sum += arr[i];
+			min = Math.min(arr[i], min);
+		}
+		sum -= min;
+		System.out.println(sum);
+		scn.close();
+	}
+}
