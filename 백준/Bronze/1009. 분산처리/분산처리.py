7705a01659abbbfcfe77b5dc7f519754d0293310
yunju-m <74498379+yunju-m@users.noreply.github.com>
2022-11-15 11:49:22 +0900

    [Bronze II] Title: 분산처리, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -0,0 +1,24 @@
+#분산처리
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for i in range(t):
+	a,b = map(int, input().split())
+	aa = a%10
+	if aa == 0:
+		print(10)
+	elif aa in [1, 5, 6]:
+		print(aa)
+	elif aa in [4, 9]:
+		bb = b%2
+		if bb == 0:
+			print(aa*aa%10)
+		else:
+			print(aa)
+	else:
+		bb = b%4
+		if bb == 0:
+			print(aa**4%10)
+		else:
+			print(aa**bb%10)		
