11a012434b25fe5a57aac57957197508164d4cbb
yunju-m <74498379+yunju-m@users.noreply.github.com>
2022-12-19 11:01:10 +0900

    [Silver II] Title: 색종이 만들기, Time: 64 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+B = [list(map(int, input().split())) for _ in range(n)]
+w_cnt, b_cnt = 0, 0
+
+def div_conq(x, y, N):
+    global w_cnt, b_cnt
+    tmp_cnt = 0
+    for i in range(x, x + N):
+        for j in range(y, y + N):
+            if B[i][j]:
+                tmp_cnt += 1
+    if not tmp_cnt:
+        w_cnt += 1
+    elif tmp_cnt == N**2:
+        b_cnt += 1
+    else:
+        div_conq(x, y, N // 2)
+        div_conq(x + N // 2, y, N // 2)
+        div_conq(x, y + N // 2, N // 2)
+        div_conq(x + N // 2, y + N // 2, N // 2)
+    return
+
+div_conq(0, 0, n)
+print(w_cnt)
+print(b_cnt)
