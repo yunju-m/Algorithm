fa53ecd7443dfe06cb0ca674ed3b40056ff1672e
yunju-m <74498379+yunju-m@users.noreply.github.com>
2023-02-16 15:02:57 +0900

    [Bronze I] Title: 캠핑, Time: 92 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/4796. 캠핑/캠핑.py
@@ -0,0 +1,12 @@
+
+cnt = 0
+while True:
+	L, P, V = map(int, input().split())
+	if L+P+V==0:
+		break
+	else:
+		vacation = 0
+		vacation += L*(V//P)
+		vacation += min((V%P), L)
+		cnt += 1
+		print(f"Case {cnt}: {vacation}")
