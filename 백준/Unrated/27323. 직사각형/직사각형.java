94b46b4d1918d6818a1a1ed70e001b8313b280ef
yunju-m <74498379+yunju-m@users.noreply.github.com>
2023-03-25 11:34:00 +0900

    [Unrated] Title: 직사각형, Time: 124 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Unrated/27323. 직사각형/직사각형.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int A, B;
+		A = Integer.parseInt(br.readLine());
+		B = Integer.parseInt(br.readLine());
+		System.out.println(A*B);;
+	}
+}
