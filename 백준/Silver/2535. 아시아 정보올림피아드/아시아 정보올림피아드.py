657e3a59d210b7a2152582299c75448db842d2e9
yunju-m <74498379+yunju-m@users.noreply.github.com>
2023-07-03 23:54:37 +0900

    [Silver V] Title: 아시아 정보올림피아드, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2535. 아시아 정보올림피아드/아시아 정보올림피아드.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(list(map(int, input().split())))
+
+arr = sorted(arr, key=lambda x: -x[2])
+print(arr[0][0], arr[0][1])
+print(arr[1][0], arr[1][1])
+
+cnt = 0
+if arr[0][0] == arr[1][0]:
+    cnt = 1
+for i in range(2, n):
+    if cnt == 0:
+        print(arr[2][0], arr[2][1])
+        break
+    else:
+        if arr[1][0] != arr[i][0]:
+            print(arr[i][0], arr[i][1])
+            break
