501e979d4b0c7e75e1ab9b4fc6ad494fdb140057
yunju-m <74498379+yunju-m@users.noreply.github.com>
2023-07-26 21:36:39 +0900

    [Silver IV] Title: 박 터뜨리기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/19939. 박 터뜨리기/박 터뜨리기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().rstrip().split())
+
+minimum = K*(K+1) // 2
+
+if minimum > N:
+    print(-1)
+elif (N - minimum) % K == 0:
+    print(K-1)
+else:
+    print(K)
