0af35827f3f9fdbbcef12e4704c99547f1fd76f7
yunju-m <74498379+yunju-m@users.noreply.github.com>
2023-08-03 21:29:51 +0900

    [Silver IV] Title: 화살표 그리기, Time: 296 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15970. 화살표 그리기/화살표 그리기.py
@@ -0,0 +1,16 @@
+n = int(input()) 
+D = {}
+for _ in range(n):
+    a,b = map(int,input().split())
+    if not b in D:
+        D[b] = [a]
+    else : D[b].append(a)
+D = D.values()
+res = 0
+for i in D:
+    i.sort()
+    if len(i) <= 1:continue
+    res += abs(i[0]-i[1])+abs(i[-1]-i[-2])
+    for j in range(1,len(i)-1):
+        res += min(abs(i[j]-i[j-1]),abs(i[j]-i[j+1]))
+print(res)
