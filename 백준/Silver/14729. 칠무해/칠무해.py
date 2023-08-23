42b193b06d62538126e2fa144b107f30dda48e08
yunju-m <74498379+yunju-m@users.noreply.github.com>
2023-08-23 17:15:28 +0900

    [Silver V] Title: 칠무해, Time: 5740 ms, Memory: 466568 KB -BaekjoonHub

---
+++ 백준/Silver/14729. 칠무해/칠무해.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+score=[]
+for i in range(N):
+	score.append(float(input()))
+score.sort()
+for i in range(7):
+	print("{:.3f}".format(score[i]))
