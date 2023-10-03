dd1b70bdfec2af1e416075aa25ee5ec55cb56a18
yunju-m <74498379+yunju-m@users.noreply.github.com>
2023-10-03 17:51:01 +0900

    [D2] Title: 간단한 369게임, Time: 126 ms, Memory: 20,596 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -1,32 +1,32 @@
 import java.util.Scanner;
 import java.io.FileInputStream;
-
+ 
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		int N = sc.nextInt();
-		String num; 
-		char temp; 
-		int cnt = 0; 
-		for (int i = 1; i <= N; i++) {
-			num = Integer.toString(i);
-			for (int j = 0; j < num.length(); j++) {
-				temp = num.charAt(j);
-				if ((int) temp % 3 == 0 && temp != '0') { 
-					cnt++;
-				}
-			}
-			if (cnt > 0) { 
-				while (cnt > 0) { 
-					sb.append('-');
-					cnt--;
-				}
-			} else { 
-				sb.append(num);
-			}
-			sb.append(' ');
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int N = sc.nextInt();
+        String num; 
+        char temp; 
+        int cnt = 0; 
+        for (int i = 1; i <= N; i++) {
+            num = Integer.toString(i);
+            for (int j = 0; j < num.length(); j++) {
+                temp = num.charAt(j);
+                if ((int) temp % 3 == 0 && temp != '0') { 
+                    cnt++;
+                }
+            }
+            if (cnt > 0) { 
+                while (cnt > 0) { 
+                    sb.append('-');
+                    cnt--;
+                }
+            } else { 
+                sb.append(num);
+            }
+            sb.append(' ');
+        }
+        System.out.println(sb);
+    }
 }
