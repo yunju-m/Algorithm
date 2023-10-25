89a00adb163975ae51807c752b55ad0fd4cf227f
yunju-m <74498379+yunju-m@users.noreply.github.com>
2023-10-25 13:47:57 +0900

    [D3] Title: 1차원 정원, Time: 311 ms, Memory: 47,996 KB -BaekjoonHub

---
+++ SWEA/D3/14178. 1차원 정원/1차원 정원.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T, n, d, x, cnt;
+		T=sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++)
+        {
+            n = sc.nextInt();
+            d = sc.nextInt();
+            cnt = 0;
+            x = d+1;
+            while (true) {
+                cnt++;
+                if (x+d < n) {
+                    x = x+d+d+1;
+                } else {
+                    break;
+                    }
+            }
+            System.out.println("#" + test_case + " " + cnt);
+		}
+	}
+}
---
+++ SWEA/D3/14178. 1차원 정원/README.md
@@ -0,0 +1,15 @@
+# [D3] 1차원 정원 - 14178 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AX_N3oSqcyUDFARi) 
+
+### 성능 요약
+
+메모리: 47,996 KB, 시간: 311 ms, 코드길이: 653 Bytes
+
+### 제출 일자
+
+2023-10-25 13:47
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
