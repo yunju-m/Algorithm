0e2da44602c2d1c6d3a6f8da431aa37181470d46
yunju-m <74498379+yunju-m@users.noreply.github.com>
2023-11-13 14:16:41 +0900

    [D3] Title: 퍼펙트 셔플, Time: 175 ms, Memory: 61,468 KB -BaekjoonHub

---
+++ SWEA/D3/3499. 퍼펙트 셔플/README.md
@@ -0,0 +1,15 @@
+# [D3] 퍼펙트 셔플 - 3499 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWGsRbk6AQIDFAVW) 
+
+### 성능 요약
+
+메모리: 61,468 KB, 시간: 175 ms, 코드길이: 496 Bytes
+
+### 제출 일자
+
+2023-11-13 14:16
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.py
@@ -0,0 +1,20 @@
+t = int(input())
+
+for tc in range(1, t + 1) :
+    n = int(input())
+    data = list(input().split())
+    if len(data) % 2 == 0 :
+        a_list = data[:len(data)//2]
+        b_list = data[len(data)//2:]
+    else :
+        a_list = data[:len(data) // 2 + 1]
+        b_list = data[len(data) // 2 + 1:]
+    result = []
+
+    while a_list or b_list :
+        if a_list :
+            result.append(a_list.pop(0))
+        if b_list :
+            result.append(b_list.pop(0))
+
+    print(f'#{tc}', *result)
