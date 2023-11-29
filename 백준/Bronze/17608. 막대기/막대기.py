179a046138dccd2fc591bce10b480729021dfcb2
yunju-m <74498379+yunju-m@users.noreply.github.com>
2023-11-29 14:46:57 +0900

    [Bronze II] Title: 막대기, Time: 96 ms, Memory: 34972 KB -BaekjoonHub

---
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+stack = []
+for i in range(N):
+    stack.append(int(input()))
+
+last = stack[-1]
+count = 1
+
+for i in reversed(range(N)):
+    if stack[i] > last:
+        count += 1
+        last = stack[i]
+print(count)
