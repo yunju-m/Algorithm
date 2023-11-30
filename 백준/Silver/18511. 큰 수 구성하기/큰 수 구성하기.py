bc3b5a4dd453940afaeaca85e82833f84e1e27ad
yunju-m <74498379+yunju-m@users.noreply.github.com>
2023-11-30 16:41:58 +0900

    [Silver V] Title: 큰 수 구성하기, Time: 48 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/18511. 큰 수 구성하기/큰 수 구성하기.py
@@ -0,0 +1,18 @@
+from itertools import product
+n, k_num = map(int, input().split())
+len_max = len(str(n))
+k = list(input().split())
+
+while True:
+    tmp = list(product(k, repeat=len_max))
+    result = []
+
+    for i in tmp:
+        if int("".join(i)) <= n:
+            result.append(int("".join(i)))
+
+    if len(result) >= 1:
+        print(max(result))
+        break
+    else:
+        len_max -= 1
